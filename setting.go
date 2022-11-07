de8c7d67fc23f3727740c460aad285dd54caa968
renyaqi <609255356@qq.com>
2022-11-07 17:27:29 +0800

    -add 新增rtmp推流实现

---
+++ setting.go
@@ -0,0 +1,26 @@
+package gortmppush
+
+import "github.com/H0RlZ0N/gortmppush/logger"
+
+// SettingFunc ...
+type SettingFunc func(*SettingEngine)
+
+// SettingEngine ...
+type SettingEngine struct {
+	loggerFactory logger.LoggerFactory
+	logLevel      logger.LogLevel
+}
+
+// WithLoggerFactory 设置日志创建类
+func WithLoggerFactory(v logger.LoggerFactory) SettingFunc {
+	return func(setting *SettingEngine) {
+		setting.loggerFactory = v
+	}
+}
+
+// WithLogLevel 设置日志等级
+func WithLogLevel(v logger.LogLevel) SettingFunc {
+	return func(setting *SettingEngine) {
+		setting.logLevel = v
+	}
+}
